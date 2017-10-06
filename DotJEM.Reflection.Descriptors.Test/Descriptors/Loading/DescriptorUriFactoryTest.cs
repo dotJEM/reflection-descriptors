9f1b243092c66d272918e50b421df851ade0b6c6
Jens Melgaard <admin@it-links.dk>
2017-10-06 15:33:33 +0200

    simple parsers for almost all

---
+++ DotJEM.Reflection.Descriptors.Test/Descriptors/Loading/DescriptorUriFactoryTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotJEM.Reflection.Descriptors.Descriptors.Loading;
+using NUnit.Framework;
+
+namespace DotJEM.Reflection.Descriptors.Test.Descriptors.Loading
+{
+    [TestFixture]
+    public class DescriptorUriFactoryTest
+    {
+        [Test]
+        public void TestAssembly()
+        {
+            // assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+            string uri = DescriptorUriFactory.CreateReference(Assembly.GetExecutingAssembly());
+            Assert.That(uri, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestType()
+        {
+            // type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+            string uri = DescriptorUriFactory.CreateReference(typeof(Fake));
+            Assert.That(uri, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestField()
+        {
+            // field://System.String myField@type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+            string uri = DescriptorUriFactory.CreateReference(typeof(Fake).GetField("myField"));
+            Assert.That(uri, Is.EqualTo(""));
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+            // field://System.String myField@type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+            string uri = DescriptorUriFactory.CreateReference(typeof(Fake).GetMethod("MyMethod"));
+
+            
+
+            Assert.That(uri, Is.EqualTo(""));
+        }
+
+        public class Fake
+        {
+            public static string myStaticField = String.Empty;
+
+            public string myField = string.Empty;
+
+            public static string MyStaticMethod(string param1, string param2) => "";
+            public string MyMethod(string param1, string param2) => "";
+            public string MyGenericMethod<T1, T2>(T1 param1, T2 param2) => "";
+        }
+    }
+
+    [TestFixture]
+    public class DescriptorUriTest
+    {
+
+        [Test]
+        public void TestType()
+        {
+            // 
+            string uri = "type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://C:\\dummy space\\My.DLL";
+            string uri2 = "method://System.String MyMethod(System.String, System.String)@type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://C:\\dummy space\\My.DLL";
+            DescriptorUri.TryParse(uri, out DescriptorUri parsed);
+
+            Assert.That(parsed, Is.TypeOf<TypeDescriptorUri>());
+
+        }
+
+        [TestCase("type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://C:\\dummy space\\My.DLL", typeof(TypeDescriptorUri))]
+        [TestCase("method://System.String MyMethod(System.String, System.String)@type://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://C:\\dummy space\\My.DLL", typeof(MethodDescriptorUri))]
+        public void TryParse_ValidUri_ReturnsExpectedType(string uri, Type expectedType)
+        {
+            DescriptorUri.TryParse(uri, out DescriptorUri parsed);
+            Assert.That(parsed, Is.TypeOf(expectedType));
+        }
+    }
+}
--- DotJEM.Reflection.Descriptors/Descriptors/Loading/DescriptorUriFactory.cs
+++ DotJEM.Reflection.Descriptors/Descriptors/Loading/DescriptorUriFactory.cs
@@ -4,7 +4,7 @@ using System.Text.RegularExpressions;
 
 namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
 {
-    internal static class DescriptorUriFactory
+    public static class DescriptorUriFactory
     {
         public static string CreateReference(this AssemblyName name)
         {
@@ -14,6 +14,7 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
 
         public static string CreateReference(this Assembly assembly)
         {
+            // assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
             //if (assembly.GlobalAssemblyCache)
             //    return "[GAC]?assembly=" + assembly.FullName;
             //return new Uri(assembly.CodeBase).LocalPath + 
@@ -23,8 +24,10 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
 
         public static string CreateReference(this Type type)
         {
-            return $"type://{type.GUID}@{CreateReference(type.Assembly)}";
-            //return CreateReference(type.Assembly) + "&type=" + type;
+            // declaringType://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+
+            return $"declaringType://{type.GUID}@{CreateReference(type.Assembly)}";
+            //return CreateReference(declaringType.Assembly) + "&declaringType=" + declaringType;
         }
 
         public static string CreateReference(this MethodInfo method)
@@ -47,7 +50,9 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
 
         public static string CreateReference(this FieldInfo field)
         {
-            return $"field://{field}@{CreateReference(field.DeclaringType)}";
+            // field://System.String myField@declaringType://e3e508c0-e442-3b15-a686-5f49daf2e138@assembly://F:\OSS\dotJEM Stack\reflection-descriptors\DotJEM.Reflection.Descriptors.Test\bin\Debug\DotJEM.Reflection.Descriptors.Test.DLL
+
+            return $"field://{field.Name}@{CreateReference(field.DeclaringType)}";
             //return CreateReference(field.DeclaringType) + "&field=" + field;
         }
 
@@ -81,6 +86,7 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
     {
         private string value;
         private DescriptorUri assembly;
+        public override DescriptorType Type => DescriptorType.Type;
 
         public TypeDescriptorUri(string value, DescriptorUri assembly)
         {
@@ -89,10 +95,76 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
         }
     }
 
+    public class MethodDescriptorUri : DescriptorUri
+    {
+        private string value;
+        private DescriptorUri type;
+        public override DescriptorType Type => DescriptorType.Method;
+
+        public MethodDescriptorUri(string value, DescriptorUri type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+    }
+
+    public class PropertyDescriptorUri : DescriptorUri
+    {
+        private string value;
+        private DescriptorUri declaringType;
+
+        public override DescriptorType Type => DescriptorType.Property;
+
+        public PropertyDescriptorUri(string value, DescriptorUri declaringType)
+        {
+            this.value = value;
+            this.declaringType = declaringType;
+        }
+    }
+
+    public class ConstructorDescriptorUri : DescriptorUri
+    {
+        private string value;
+        private DescriptorUri type;
+        public override DescriptorType Type => DescriptorType.Constructor;
+
+        public ConstructorDescriptorUri(string value, DescriptorUri type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+    }
+
+    public class EventDescriptorUri : DescriptorUri
+    {
+        private string value;
+        private DescriptorUri type;
+        public override DescriptorType Type => DescriptorType.Event;
+
+        public EventDescriptorUri(string value, DescriptorUri type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+    }
+
+    public class FieldDescriptorUri : DescriptorUri
+    {
+        private string value;
+        private DescriptorUri type;
+        public override DescriptorType Type => DescriptorType.Field;
+
+        public FieldDescriptorUri(string value, DescriptorUri type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+    }
+
     public abstract class DescriptorUri
     {
         public abstract DescriptorType Type { get; }
-        private static readonly Regex pattern = new Regex("(?'type'\\w+)\\:\\/\\/(?'val'[^@])(@(?'at'.*))?", 
+        private static readonly Regex pattern = new Regex("(?'declaringType'\\w+)\\:\\/\\/(?'val'[^@]+)(@(?'at'.*))?", 
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         public static bool TryParse(string str, out DescriptorUri result)
@@ -102,46 +174,83 @@ namespace DotJEM.Reflection.Descriptors.Descriptors.Loading
             if (!match.Success)
                 return false;
 
-            switch (match.Groups["type"].Value.ToLowerInvariant())
+            bool TryParsePart(out DescriptorUri outpart)
+            {
+                outpart = null;
+                string typePart = match.Groups["at"].Value;
+                return !string.IsNullOrEmpty(typePart) && TryParse(typePart, out outpart);
+            }
+
+            DescriptorUri part = null;
+            switch (match.Groups["declaringType"].Value.ToLowerInvariant())
             {
                 case "assembly":
                     result = new AssemblyDescriptorUri(match.Groups["val"].Value);
                     return true;
-                case "type":
-                    var at = match.Groups["at"].Value;
-                    if (string.IsNullOrEmpty(at))
+                case "declaringType":
+                    if (!TryParsePart(out part))
                         return false;
 
-                    if (!TryParse(at, out DescriptorUri assembly))
-                        return false;
-
-                    result = new TypeDescriptorUri(match.Groups["val"].Value, assembly);
+                    result = new TypeDescriptorUri(match.Groups["val"].Value, part);
                     return true;
                 case "method":
+                    if (!TryParsePart(out part))
+                        return false;
+
+                    result = new MethodDescriptorUri(match.Groups["val"].Value, part);
+                    return true;
+
                 case "constructor":
+                    if (!TryParsePart(out part))
+                        return false;
+
+                    result = new ConstructorDescriptorUri(match.Groups["val"].Value, part);
+                    return true;
+
                 case "property":
+                    if (!TryParsePart(out part))
+                        return false;
+
+                    result = new PropertyDescriptorUri(match.Groups["val"].Value, part);
+                    return true;
+
+
                 case "field":
+                    if (!TryParsePart(out part))
+                        return false;
+
+                    result = new FieldDescriptorUri(match.Groups["val"].Value, part);
+                    return true;
+
                 case "event":
+                    if (!TryParsePart(out part))
+                        return false;
+
+                    result = new EventDescriptorUri(match.Groups["val"].Value, part);
                     return true;
             }
             return false;
-
-            //Assembly,
-            //Type,
-            //Module,
-            //Method,
-            //Property,
-            //Field,
-            //Event,
-            //Constructor,
-            //Attribute,
-            //Object,
-            //Invalid
-
-
-
-
         }
+
+        public static DescriptorUri Parse(string uri)
+        {
+            if (TryParse(uri, out DescriptorUri result))
+                return result;
+
+            throw new UriFormatException("Invalid descriptor uri format.");
+        }
+
+        //Assembly,
+        //Type,
+        //Module,
+        //Method,
+        //Property,
+        //Field,
+        //Event,
+        //Constructor,
+        //Attribute,
+        //Object,
+        //Invalid    
     }
 
 }
